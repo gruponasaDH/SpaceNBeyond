28b4fa17a60f025e1411e7740a0cdffa94df96ef
ericaide <loriped2236>
2020-02-05 03:58:19 -0300

    splash layout base

---
+++ app/src/main/java/com/example/spacenbeyond/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.spacenbeyond;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static int splashTimeOut = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        }, splashTimeOut);
+    }
+
+}
