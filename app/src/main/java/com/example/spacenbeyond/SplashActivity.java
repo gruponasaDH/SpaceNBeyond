571284b4ee268e9e6abed0098a87a2289db14f00
ericaide <loriped2236>
2020-02-05 22:25:34 -0300

    splash layout base com imagem

--- app/src/main/java/com/example/spacenbeyond/SplashActivity.java
+++ app/src/main/java/com/example/spacenbeyond/SplashActivity.java
@@ -8,7 +8,7 @@ import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private static int splashTimeOut = 4000;
+    private static int splashTimeOut = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
