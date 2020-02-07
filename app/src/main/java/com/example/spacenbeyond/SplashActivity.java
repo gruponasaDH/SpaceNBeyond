43d43dcfa0f0cba344c2ffb7803622b79278cbf8
ericaide <loriped2236>
2020-02-06 23:53:02 -0300

    splash fullscreen

--- app/src/main/java/com/example/spacenbeyond/SplashActivity.java
+++ app/src/main/java/com/example/spacenbeyond/SplashActivity.java
@@ -5,14 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private static int splashTimeOut = 3000;
+    private static int splashTimeOut = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
 
         new Handler().postDelayed(new Runnable() {
