98fe2a2802d7f03d9c30d251ab01e4619d6a9524
Londrinho <jimmyapara@gmail.com>
2020-02-10 11:09:02 -0300

    Merge branches 'jimmy/cdastro' and 'pre_epic' of https://github.com/gruponasaDH/SpaceNBeyond into jimmy/cdastro
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/spacenbeyond/TelaUsuario.java
    #	app/src/main/res/layout/activity_main.xml
    #	app/src/main/res/values/strings.xml
    #	build.gradle
    #	gradle/wrapper/gradle-wrapper.properties

--- app/src/main/java/com/example/spacenbeyond/MainActivity.java
+++ app/src/main/java/com/example/spacenbeyond/MainActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class TelaUsuario extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tela_usuario);
+        setContentView(R.layout.activity_main);
     }
 }
