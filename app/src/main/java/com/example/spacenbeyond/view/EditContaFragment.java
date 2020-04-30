321b441a876a9761c9a8d7e9c545aa3aeb609ba5
ericaide <loriped2236>
2020-04-29 22:26:11 -0300

    logout google e facebook

--- app/src/main/java/com/example/spacenbeyond/view/EditContaFragment.java
+++ app/src/main/java/com/example/spacenbeyond/view/EditContaFragment.java
@@ -2,6 +2,8 @@ package com.example.spacenbeyond.view;
 
 import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
@@ -9,13 +11,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.spacenbeyond.R;
+import com.facebook.login.LoginManager;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class EditContaFragment extends Fragment {
 
     private ImageView imageViewSair;
+    private TextView textoLogout;
+    private GoogleSignInClient googleSignInClient;
 
     public EditContaFragment() { }
 
@@ -25,21 +36,49 @@ public class EditContaFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_edit_conta, container, false);
 
-        imageViewSair = view.findViewById(R.id.imageViewLogout);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        googleSignInClient = GoogleSignIn.getClient(getActivity(), gso);
+
+        initViews(view);
+
 
         imageViewSair.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                logout();
+                logout(googleSignInClient);
+            }
+        });
+
+        textoLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logout(googleSignInClient);
             }
         });
 
         return view;
     }
 
-    private void logout() {
-        FirebaseAuth.getInstance().signOut();   
-        Intent intent = new Intent(getActivity(), LoginActivity.class);
-        startActivity(intent);
+    private void initViews(View view){
+        imageViewSair = view.findViewById(R.id.imageViewLogout);
+        textoLogout = view.findViewById(R.id.textViewLogout);
+
     }
+
+
+    private void logout(GoogleSignInClient googleSignInClient) {
+        googleSignInClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                LoginManager.getInstance().logOut();
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
 }
